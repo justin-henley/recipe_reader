6fdcb72ba278c860c23bec97c941339e56895ca2
justin-henley <justin.a.henley@gmail.com>
2022-03-01 13:50:41 +0800

    added getters, setters, and JS Docs comments

--- js/Voice.js
+++ js/Voice.js
@@ -1,15 +1,36 @@
+/**
+ * Class for accessing the Responsive Voice API
+ * @extends Object
+ */
 export default class Voice extends Object {
+  // Properties
   #speechQueue;
   #voiceProfile;
-  busy;
+  #busy;
+  #voices = [];
 
+  /**
+   * Create a voice object
+   * @constructor
+   * @param {string} voiceProfile - The voice to use when reading
+   */
   constructor(voiceProfile = "UK English Male") {
     super();
     this.#speechQueue = [];
     this.#voiceProfile = voiceProfile;
-    this.busy = false;
+    responsiveVoice.setDefaultVoice(this.#voiceProfile);
+    this.#busy = false;
+
+    // Unpack voice names from array of objects
+    for (let obj of responsiveVoice.getVoices()) {
+      this.#voices.push(obj.name);
+    }
   }
 
+  /**
+   * Enqueues a piece of text to be read
+   * @param {string} text - The text to be read aloud
+   */
   speak(text) {
     // Push text to speech queue
     if (text) this.#speechQueue.push(text);
@@ -18,20 +39,48 @@ export default class Voice extends Object {
     this.read();
   }
 
+  /**
+   * Reads the first item in the queue
+   */
   read() {
-    if (!this.busy && this.#speechQueue.length > 0) {
+    if (!this.#busy && this.#speechQueue.length > 0) {
       // Lock speech
-      this.busy = true;
+      this.#busy = true;
 
       // Read the first entry in the queue
       responsiveVoice.speak(this.#speechQueue[0], this.#voiceProfile, {
         onend: () => {
           this.#speechQueue.shift();
-          this.speak();
+          this.read();
         },
       });
 
-      this.busy = false;
+      this.#busy = false;
+    }
+  }
+
+  /**
+   * @returns {Array} - The list of available voices from Responsive Voice API
+   */
+  get availableVoices() {
+    return this.#voices;
+  }
+
+  /**
+   * Gets the current voice profile
+   * @returns {string} - The current voice profile
+   */
+  get voiceProfile() {
+    return this.#voiceProfile;
+  }
+
+  /**
+   * Sets the voice profile
+   * @param {string} voice - The voice profile for the reader to use
+   */
+  set voiceProfile(voice) {
+    if (this.#voices.includes(voice)) {
+      this.#voiceProfile = voice;
     }
   }
 }
